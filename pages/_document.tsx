6693ac307be09f3a2ea3e8956961cd8e7a801827
Muhammad Fakhri Imaduddin <fakhri.imaduddin@ruangguru.com>
2022-07-11 23:27:35 +0700

    refactor: refactor code and add prettier

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,34 +1,23 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
-function MyDocument() {
+const MyDocument = () => {
   return (
     <Html>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
         <link
-          href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
-          rel="stylesheet"
+          href='https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap'
+          rel='stylesheet'
         />
       </Head>
-      <body style={{ fontFamily: "Nunito Sans, sans-serif", height: "100%" }}>
+      <body style={{ fontFamily: 'Nunito Sans, sans-serif', height: '100%' }}>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
-}
-
-export async function getInitialProps(ctx: DocumentContext) {
-  const initialProps = await Document.getInitialProps(ctx);
-  return { ...initialProps };
-}
+};
 
 export default MyDocument;
