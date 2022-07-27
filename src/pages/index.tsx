54e998e56a94cbb51315d004f3e97aec669301db
Alif Alamsyah <alif.alamsyah@ruangguru.com>
2022-07-27 20:02:13 +0700

    [land] chore: refactor folder

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next';
+import HomeContainer from '../uikit/Home/HomeContainer';
+
+const Home: NextPage = () => {
+  return <HomeContainer />;
+};
+
+export default Home;
