c7fabc176adf300378f387706e9d46f43efc6afc
Muhammad Fakhri Imaduddin <fakhri.imaduddin@ruangguru.com>
2022-08-22 22:10:00 +0700

    feat: set env var at github action

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   publicRuntimeConfig: {
     showVersion: process.env.SHOW_VERSION === 'true' || false,
-    clientVersion: process.env.VERSION_DIGEST || 'local-0.0.1',
+    clientVersion: process.env.COMMIT_HASH || 'local-0.0.1',
   },
 };
 
