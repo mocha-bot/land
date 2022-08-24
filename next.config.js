4f42d2b66b3afa8335c935bfc8ce355dc4161de7
Muhammad Fakhri Imaduddin <fakhri.imaduddin@ruangguru.com>
2022-08-24 21:59:27 +0700

    feat: add cache to github action and add deployment webhook (#21)
    * feat: copy action from template
    
    * feat: remove instal using lockfile
    
    * feat: show version to make debugging easier
    
    * feat: set env var at github action
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: test send webhook
    
    * feat: done
    
    * feat: remove .env file
    
    * feat: change node version to lts
    
    * feat: make bottom css conditional
    
    * feat: move domain to github action secrets
    
    * refactor: refactor next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  publicRuntimeConfig: {
+    showVersion: process.env.SHOW_VERSION === 'TRUE',
+    clientVersion: process.env.COMMIT_HASH || 'local-0.0.1',
+  },
 };
 
 module.exports = nextConfig;
