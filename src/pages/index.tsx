18be85084526beaf4f58c7d927c597f3bb05ef4b
Muhammad Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2023-05-13 23:23:43 +0700

    feat: change back translation lib to use next-i18next

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
+import type { GetStaticPropsContext } from 'next';
+import { i18n } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import getConfig from 'next/config';
+
 import { HomeContainer } from '@/uikit/Home/HomeContainer';
+
+const { publicRuntimeConfig } = getConfig();
 
 function Home() {
   return <HomeContainer />;
 }
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  if (!publicRuntimeConfig.isProduction) {
+    await i18n?.reloadResources();
+  }
+
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+    },
+  };
+}
+
 export default Home;
