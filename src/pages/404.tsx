4c5f56e14346e30099b8d9d22f4c9aafa6028013
aldiwildan77 <aldiwild77@gmail.com>
2024-10-11 14:43:46 +0700

    feat: add i18n at not found page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,28 +1,152 @@
-import { Box, Flex, Image, Text, useMediaQuery } from '@chakra-ui/react';
+/* eslint-disable react/no-unstable-nested-components */
+
+import { Box, Flex, Image, Text, useBreakpointValue } from '@chakra-ui/react';
+import type { GetStaticPropsContext } from 'next';
+import { i18n, useTranslation } from 'next-i18next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
 import Footer from '@/uikit/Footer';
 import Header from '@/uikit/Header';
 
-const descriptions = {
-  mobile: () => (
-    <Text as='h3' fontWeight='light'>
-      Whoops, you’ve wandered far, even Pluto is in your rearview! Seems like
-      the page you’re looking for has drifted off-course
-    </Text>
-  ),
-  desktop: () => (
-    <>
-      <Text as='h3' fontWeight='light'>
-        Whoops, you’ve wandered far, even Pluto is in your rearview!
-      </Text>
-      <Text>Seems like the page you’re looking for has drifted off-course</Text>
-    </>
-  ),
+import { publicRuntimeConfig } from '../../next.config';
+
+type PlanetBackgroundProps = {
+  isMobile?: boolean;
 };
 
+function PlanetBackground({ isMobile }: PlanetBackgroundProps) {
+  if (isMobile) {
+    return (
+      <>
+        {/* planet left */}
+        <Image
+          src='/assets/images/planet_3.svg'
+          alt='planet'
+          position='absolute'
+          left={{ base: '-25', md: 'none' }}
+          bottom={{ base: '-10', md: 'none' }}
+          overflow='hidden'
+          zIndex={3}
+          loading='lazy'
+        />
+        {/* planet right */}
+        <Image
+          src='/assets/images/planet_4.svg'
+          alt='planet'
+          w={{ base: 'full', md: '70%' }}
+          position='absolute'
+          right={{ base: '-25', md: 'none' }}
+          top={{ base: '-10', md: 'none' }}
+          overflow='hidden'
+          zIndex={3}
+          loading='lazy'
+        />
+        {/* Flare top */}
+        <Image
+          src='/assets/images/flare_5.svg'
+          alt='flare'
+          h='full'
+          w='full'
+          position='absolute'
+          top={{ base: '-205', md: 'none' }}
+          zIndex={3}
+          loading='lazy'
+        />
+        {/* Flare bottom */}
+        <Image
+          src='/assets/images/flare_4.svg'
+          alt='flare'
+          w='full'
+          position='absolute'
+          bottom={{ base: '-90', md: 'none' }}
+          zIndex={3}
+          loading='lazy'
+        />
+      </>
+    );
+  }
+
+  return (
+    <>
+      {/* planet left */}
+      <Image
+        src='/assets/images/planet_1.svg'
+        alt='planet'
+        position='absolute'
+        left={{ base: 'none', md: '-105' }}
+        bottom={{ base: 'none', md: '-125' }}
+        overflow='hidden'
+        zIndex={3}
+        loading='lazy'
+      />
+      {/* planet right */}
+      <Image
+        src='/assets/images/planet_2.svg'
+        alt='planet'
+        w={{ base: 'full', md: '70%' }}
+        position='absolute'
+        right={{ base: 'none', md: '-125' }}
+        top={{ base: 'none', md: '-105' }}
+        overflow='hidden'
+        zIndex={3}
+        loading='lazy'
+      />
+      {/* Flare top */}
+      <Image
+        src='/assets/images/flare_2.svg'
+        alt='flare'
+        h='full'
+        w='full'
+        position='absolute'
+        top={{ base: 'none', md: '-305', lg: '-105' }}
+        zIndex={3}
+        loading='lazy'
+      />
+      {/* Flare bottom */}
+      <Image
+        src='/assets/images/flare_1.svg'
+        alt='flare'
+        w='full'
+        position='absolute'
+        bottom={{ base: 'none', md: '-305' }}
+        zIndex={3}
+        loading='lazy'
+      />
+    </>
+  );
+}
+
+type NotFoundDescriptionProps = {
+  isMobile?: boolean;
+  descriptions?: string[];
+};
+
+function NotFoundDescription({
+  isMobile,
+  descriptions = [],
+}: NotFoundDescriptionProps) {
+  if (isMobile) {
+    return (
+      <Text as='h3' fontWeight='light'>
+        {descriptions}
+      </Text>
+    );
+  }
+
+  return (
+    <>
+      {descriptions.map((desc) => (
+        <Text key={desc} as='h3' fontWeight='light'>
+          {desc}
+        </Text>
+      ))}
+    </>
+  );
+}
+
 export default function Page404() {
-  const [isMobile] = useMediaQuery('(max-width: 768px)');
-  const [isDesktop] = useMediaQuery('(min-width: 1024px)');
+  const { t } = useTranslation();
+  const isMobile = useBreakpointValue({ base: true, lg: false });
 
   return (
     <Flex flexDirection='column'>
@@ -48,113 +172,35 @@ export default function Page404() {
             as='h1'
             fontWeight='bold'
             fontSize={{ base: '9xl', lg: '10xl' }}>
-            404
+            {t('common:not_found.title')}
           </Text>
           <Box
             fontSize={{ base: 'lg', md: '2xl', lg: '3xl' }}
             textAlign={{ base: 'center' }}>
-            {isMobile ? <descriptions.mobile /> : <descriptions.desktop />}
+            <NotFoundDescription
+              isMobile={isMobile}
+              descriptions={t('common:not_found.descriptions', {
+                returnObjects: true,
+              })}
+            />
           </Box>
         </Flex>
-        {isDesktop && (
-          <>
-            {/* planet left */}
-            <Image
-              src='/assets/images/planet_1.svg'
-              alt='planet'
-              position='absolute'
-              left={{ base: 'none', md: '-105' }}
-              bottom={{ base: 'none', md: '-125' }}
-              overflow='hidden'
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* planet right */}
-            <Image
-              src='/assets/images/planet_2.svg'
-              alt='planet'
-              w={{ base: 'full', md: '70%' }}
-              position='absolute'
-              right={{ base: 'none', md: '-125' }}
-              top={{ base: 'none', md: '-105' }}
-              overflow='hidden'
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* Flare top */}
-            <Image
-              src='/assets/images/flare_2.svg'
-              alt='flare'
-              h='full'
-              w='full'
-              position='absolute'
-              top={{ base: 'none', md: '-305', lg: '-105' }}
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* Flare bottom */}
-            <Image
-              src='/assets/images/flare_1.svg'
-              alt='flare'
-              w='full'
-              position='absolute'
-              bottom={{ base: 'none', md: '-305' }}
-              zIndex={3}
-              loading='lazy'
-            />
-          </>
-        )}
 
-        {isMobile && (
-          <>
-            {/* planet left */}
-            <Image
-              src='/assets/images/planet_3.svg'
-              alt='planet'
-              position='absolute'
-              left={{ base: '-25', md: 'none' }}
-              bottom={{ base: '-10', md: 'none' }}
-              overflow='hidden'
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* planet right */}
-            <Image
-              src='/assets/images/planet_4.svg'
-              alt='planet'
-              w={{ base: 'full', md: '70%' }}
-              position='absolute'
-              right={{ base: '-25', md: 'none' }}
-              top={{ base: '-10', md: 'none' }}
-              overflow='hidden'
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* Flare top */}
-            <Image
-              src='/assets/images/flare_5.svg'
-              alt='flare'
-              h='full'
-              w='full'
-              position='absolute'
-              top={{ base: '-205', md: 'none' }}
-              zIndex={3}
-              loading='lazy'
-            />
-            {/* Flare bottom */}
-            <Image
-              src='/assets/images/flare_4.svg'
-              alt='flare'
-              w='full'
-              position='absolute'
-              bottom={{ base: '-90', md: 'none' }}
-              zIndex={3}
-              loading='lazy'
-            />
-          </>
-        )}
+        <PlanetBackground isMobile={isMobile} />
       </Flex>
       <Footer />
     </Flex>
   );
+}
+
+export async function getStaticProps({ locale }: GetStaticPropsContext) {
+  if (!publicRuntimeConfig.isProduction) {
+    await i18n?.reloadResources();
+  }
+
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+    },
+  };
 }
