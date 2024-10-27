f6212c2eff17b4227e525ae4cbdfd963a407fbcf
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2024-10-27 23:53:05 +0700

    feat: change throwed error to be string

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -33,7 +33,7 @@ export const axios: Axios = (config, schema) => {
         if (err instanceof ZodError) {
           reject({
             _type: 'DECODE_ERROR',
-            message: JSON.parse(JSON.stringify(err.errors)),
+            message: JSON.stringify(err.errors),
           });
         }
         if (err instanceof AxiosError) {
