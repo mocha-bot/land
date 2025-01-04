001b162db03727abcd2b5c77b4b75301c402c49c
Fakhri Imaduddin <fakhriimaduddin19@gmail.com>
2025-01-04 14:57:28 +0700

    feat: add room detail page (ui only)

--- src/modules/room/roomEntity.ts
+++ src/modules/room/roomEntity.ts
@@ -26,3 +26,48 @@ export type Room = {
   rate: number;
   tags: string[];
 };
+
+export const DUMMY_ROOM = {
+  serial: '1',
+  code: 'ROOM-OWVUMGGVR',
+  name: 'Mocha',
+  description:
+    'Mocha is a chat application Lorem ipsum dolor sit amet consectetur. Justo quam imperdiet fermentum cursus vel pellentesque. Quis rhoncus eu nisi elit turpis. In odio felis nisl molestie. Habitant eget ac diam molestie gravida eu vel ipsum. Ut non fermentum tempor quam eu orci id tempor. Quis sed consectetur urna purus scelerisque ultricies ultricies. Eleifend in facilisis ac senectus. Sit id augue ullamcorper arcu quam convallis imperdiet gravida. Nunc mauris velit dictum tortor maecenas vitae. Dictum magnis bibendum habitasse nibh urna at sed. Viverra nunc tincidunt scelerisque nulla ut orci tortor diam quis. Enim etiam pellentesque lectus ipsum in eget ornare volutpat gravida. Semper nisl vivamus egestas sed. Sit adipiscing pellentesque tortor a lectus vulputate vitae.',
+  serverJoined: 1000,
+  rate: 4.5,
+  totalReview: 1000,
+  serverBanner: '/assets/images/detail-room-server-banner.png',
+  tags: ['chat', 'application'],
+  createdAt: '2023-03-01T00:00:00Z',
+  language: {
+    code: 'en',
+    name: 'English',
+  },
+  serverOwner: {
+    name: 'Chroma Hub',
+    image: '/assets/images/logo-mocha.png',
+    owner: 'Aldiwild#0001',
+  },
+  servers: [
+    {
+      image: '/assets/images/logo-mocha.png',
+      name: 'Chroma Devs 1',
+      owner: 'Aldiwild#0001',
+    },
+    {
+      image: '/assets/images/logo-mocha.png',
+      name: 'Chroma Devs 2',
+      owner: 'Aldiwild#0001',
+    },
+    {
+      image: '/assets/images/logo-mocha.png',
+      name: 'Chroma Devs 3',
+      owner: 'Aldiwild#0001',
+    },
+    {
+      image: '/assets/images/logo-mocha.png',
+      name: 'Chroma Devs 4',
+      owner: 'Aldiwild#0001',
+    },
+  ],
+};
