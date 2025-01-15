b316273d742cf1d686b3a7ee4391cd6027ae39ea
aldiwildan77 <aldiwild77@gmail.com>
2025-01-16 01:52:02 +0700

    feat: add tawk to

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,14 @@ import Head from 'next/head';
 import { GoogleAnalytics } from 'nextjs-google-analytics';
 
 import { AppProvider } from '@/AppProvider';
+import TawkMessenger from '@/components/TawkMessenger/TawkMessenger';
 import { fonts } from '@/theme/fonts';
 
 import '@/theme/global.css';
 
 const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_TRACKING_ID || '';
+const TAWK_PROPERTY_ID = process.env.TAWK_PROPERTY_ID || '';
+const TAWK_WIDGET_ID = process.env.TAWK_WIDGET_ID || '';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -26,6 +29,12 @@ function App({ Component, pageProps }: AppProps) {
       </Head>
       <GoogleAnalytics trackPageViews gaMeasurementId={GA_TRACKING_ID} />
       <AppProvider>
+        {TAWK_PROPERTY_ID && TAWK_WIDGET_ID && (
+          <TawkMessenger
+            propertyId={TAWK_PROPERTY_ID}
+            widgetId={TAWK_WIDGET_ID}
+          />
+        )}
         <Component {...pageProps} />
         <Analytics />
       </AppProvider>
